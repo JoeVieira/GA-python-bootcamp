0d7935b8806ffb310c82a3e50cbe94d4af6553c4
JoeVieira <joe.vieira@gmail.com>
2018-03-09 18:02:27 -0500

    robustness checks

---
+++ robust.py
@@ -0,0 +1,10 @@
+age = int(input("Please enter your age: "))
+
+while(True):
+    try:
+        age = int(input("Please enter your age: "))
+    except ValueError:
+        print("Sorry, I didn't understand that.")
+        continue
+    else:
+        break
