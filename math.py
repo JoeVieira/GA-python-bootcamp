dc15bcd5c388b176ed932e31c1f76b65e66a0645
JoeVieira <joe.vieira@gmail.com>
2018-03-09 17:35:36 -0500

    initial commit of class material

---
+++ math.py
@@ -0,0 +1,24 @@
+import decimal
+
+testNumber = 5
+
+newNumber = testNumber + 5
+
+print(newNumber)
+
+newNumber = testNumber * 5
+
+print(newNumber)
+
+newNumber = newNumber / 5  # returns a float
+
+print(newNumber)
+
+newNumber = testNumber % 1
+
+print(newNumber)
+
+newNumber = testNumber ** 2
+
+print(newNumber)
+
---
+++ strings.py
@@ -0,0 +1,9 @@
+str1 = "Hello"
+str2 = 'Hello'
+str3 = "Hello\tworld\n"
+str4 = str1 + " world"
+str5 = str1 + "4"
+
+print(str3)
+print(str4)
+print(str5)
